4cab7256173b7dd3e3ec556e1b17c92b9ac1effa
Julian-Cto <juliancerrato1@gmail.com>
2024-04-20 12:38:14 -0700

    Starter code

--- ComplexPlane.cpp
+++ ComplexPlane.cpp
@@ -1,3 +1,50 @@
 #include "ComplexPlane.h"
 
+ComplexPlane::ComplexPlane(int pixelWidth, int pixelHeight)
+{
+
+}
+void ComplexPlane::draw(RenderTarget& target, RenderStates states)const
+{
+
+}
+void ComplexPlane::zoomin()
+{
+
+}
+void ComplexPlane::zoomOut()
+{
+
+}
+void ComplexPlane::setCenter(Vector2i mousePixel)
+{
+
+}
+void ComplexPlane::setMouseLocation(Vector2i mousePixel)
+{
+
+}
+void ComplexPlane::loadText(Text& text)
+{
+
+}
+void ComplexPlane::updateRender()
+{
+
+}
+int ComplexPlane::countIterations(Vector2f coord)
+{
+	return 0;
+}
+void ComplexPlane::iterationsToRGB(size_t count, Uint8& r, Uint8& g, Uint8& b)
+{
+
+}
+Vector2f ComplexPlane::mapPixelToCoords(Vector2i mousePixel)
+{
+	Vector2f test;
+	return test;
+}
+
+
 
--- main.cpp
+++ main.cpp
@@ -2,9 +2,31 @@
 
 int main() 
 {
-	// handle input
 
-	// update scene
+	VideoMode vm(1920, 1080);
 
-	// draw scene
+	RenderWindow window(vm, "Mandelbrot", Style::Default);
+
+	while (window.isOpen())
+	{
+		// handle input
+		Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == Event::Closed)
+			{
+				window.close();
+			}
+		}
+		if (Keyboard::isKeyPressed(Keyboard::Escape))
+		{
+			window.close();
+		}
+		// update scene
+		
+		// draw scene
+		window.clear();
+
+		window.display();
+	}
 }
