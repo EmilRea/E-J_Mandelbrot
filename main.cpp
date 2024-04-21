b3885f955edb83240e15311a5cbbc9c7f3d18f72
EmilRea <166642391+EmilRea@users.noreply.github.com>
2024-04-20 22:27:57 -0700

    Update1 main.cpp

--- main.cpp
+++ main.cpp
@@ -17,13 +17,22 @@ int main()
 			{
 				window.close();
 			}
+			else if (event.type == Event::MouseButtonPressed)
+            {
+                if (event.mouseButton.button == Mouse::Left)
+                {
+                    Vector2f complexCoord = complexPlane.mapPixelToCoords({ event.mouseButton.x, event.mouseButton.y });
+                    cout << "Clicked at complex coordinate: (" << complexCoord.x << ", " << complexCoord.y << ")" << endl;
+                }
+            }
 		}
 		if (Keyboard::isKeyPressed(Keyboard::Escape))
 		{
 			window.close();
 		}
 		// update scene
-		
+		complexPlane.updateRender();
+
 		// draw scene
 		window.clear();
 
